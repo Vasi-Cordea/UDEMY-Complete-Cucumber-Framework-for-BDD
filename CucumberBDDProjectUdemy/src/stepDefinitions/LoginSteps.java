e57dae31696fd931330bbd9926ee3db35b2bd951
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-03-18 09:51:53 +0200

    driver actions on page

--- CucumberBDDProjectUdemy/src/stepDefinitions/LoginSteps.java
+++ CucumberBDDProjectUdemy/src/stepDefinitions/LoginSteps.java
@@ -1,5 +1,7 @@
 package stepDefinitions;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,11 +10,22 @@ public class LoginSteps {
 	
 	//Use Cucumber to bind the Feature File step to Java code
 	// Glue code - step definitions - step implementation
-
+WebDriver driver;
 
 	@Given("the user is on the login page")
 	public void user_on_login_page() {
 		System.out.println("user is on the login page");
+		
+		//initiate the driver
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vasic\\Documents\\Jars\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	
+
+	
+
+		
+		
+		
 	}
 		@When("the user enters valid credentials")
 		public void user_enters_valid_credentials(){
