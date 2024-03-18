4bbc144faf91dfdc2535c088ae9522e4582d7b79
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-03-18 10:59:42 +0200

    backend feature and @ingone tags

--- CucumberBDDProjectUdemy/src/runners/LogginRunner.java
+++ CucumberBDDProjectUdemy/src/runners/LogginRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features = "projectfeatures", 
 		glue = "stepDefinitions",
-        tags = "@goodlogin"
+        tags = {"endtoend", "@goodlogin", "~@ignore"}
 
 )
 public class LogginRunner {
